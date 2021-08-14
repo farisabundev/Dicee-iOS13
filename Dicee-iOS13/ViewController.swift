38773f0685f4451237c732d22ae9d2db1cd84efa
Faris Imaduddin <faris.imaduddin@koala.id>
2021-08-15 00:18:36 +0700

    Course Complete

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // IBOutlet is reference for the UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    // initial value
+    let imageArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+    
+    // IBAction is reference from element to code
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+		
+        let leftDiceNumber = Int.random(in: 0..<5)
+        let rightDiceNumber = Int.random(in: 0..<5)
+
+        diceImageView1.image = imageArray[leftDiceNumber]
+        diceImageView2.image = imageArray[rightDiceNumber]
+        
+        // optional solution to shuffle array/random
+        // diceImageView1.image = imageArray.randomElement()
+        // diceImageView2.image = imageArray.randomElement()
+
+        // var firstIndex = Int.random(in: 0...5)
+        // var secondIndex = Int.random(in: 0...5)
+
+        // func shuffleAgain() {
+        //     firstIndex = Int.random(in: 0...5)
+        //     secondIndex = Int.random(in: 0...5)
+
+        //     // call function to re-shuffle
+        //     shuffle()
+        // }
+
+        // func shuffle() {
+        //     if firstIndex == secondIndex {
+        //         shuffleAgain()
+        //     } else {
+        //         print("stop, got the result")
+        //         diceImageView1.image = imageArray[firstIndex]
+        //         diceImageView2.image = imageArray[secondIndex]
+        //     }
+        // }
+
+        // shuffle()
     }
-
-
+    
 }
 
